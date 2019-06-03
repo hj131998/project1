0e078393325b73e5a93b37f1e584060d3afdfa85
hj131998 <50947992+hj131998@users.noreply.github.com>
2019-06-03 14:43:53 +0530

    Update ex.sql

--- ex.sql
+++ ex.sql
@@ -1,3 +1,3 @@
 spool c:\Users\itiwari\desktop\abc.txt
-create table xgap2(id INT PRIMARY KEY,name varchar(20));
+create table xgap3(id INT PRIMARY KEY,name varchar(20));
 spool off;                                                   
